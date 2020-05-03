eb9183b80aef456e0bdeaf170d4888dfc6421a05
Bryce Kennedy <btken88@gmail.com>
2020-05-03 19:03:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,17 +30,6 @@ def exit_jukebox
   puts "Goodbye"
   return
 end
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
 
 def run(songs)
   command = ""
